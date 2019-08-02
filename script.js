717690f7987c15adeb20333bef272f38517a6683
David Johnny <minotaurlynx@gmail.com>
2019-08-02 15:16:04 -0400

    oh

---
+++ script.js
@@ -0,0 +1,12 @@
+function replaceImg(){
+    let img = document.getElementById('switch');
+    img.src = "mainb.png";
+    // img.remove();
+    
+    // let newImg = document.createElement('img');
+    // let mainContainer = document.querySelector('.main-container');
+    
+    // mainContainer.appendChild(newImg);
+    // newImg.src= "mainb.PNG";
+    }
+
